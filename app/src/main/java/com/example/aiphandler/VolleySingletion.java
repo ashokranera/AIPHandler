a06f3b963021e65691f25c016c9a17672886a7ec
ashokranera <ashokranera@gmail.com>
2020-02-04 15:27:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aiphandler/VolleySingletion.java
@@ -0,0 +1,27 @@
+package com.example.aiphandler;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleySingletion {
+
+    private static  VolleySingletion mInstance;
+    private RequestQueue mRequestQueue;
+
+    public VolleySingletion(Context context) {
+
+        mRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
+    }
+
+    public static synchronized  VolleySingletion getInstance(Context context){
+        if(mInstance ==null){
+            mInstance = new VolleySingletion(context);
+        }
+        return mInstance;
+    }
+    public RequestQueue getRequestQueue(){
+        return mRequestQueue;
+    }
+}
